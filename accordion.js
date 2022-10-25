62339b0cd49966065f530ddc5e4258421f366d95
Sergey <66194887+GMDdesign@users.noreply.github.com>
2022-10-25 12:23:50 +0700

    Create accordion.js
    accordion.js

---
+++ accordion.js
@@ -0,0 +1,43 @@
+<script src="./libs/jquery.js"></script>
+<!-- accordion -->
+<script>
+    if($('.msg-user').length){
+        $( ".notithing" ).remove();
+    }
+    if($('.msg-cont2').length){
+        $( ".notithing" ).remove();
+    }
+    if($('.msg-user').length){
+        $( ".notithing-own" ).remove();
+    }
+    if($('.msg-cont2').length){
+        $( ".notithing-own" ).remove();
+    }
+    $('textarea').keyup(function() {
+    $( ".notithing" ).remove();
+ }); // удаление текста
+
+ if($( ".notithing-own" ).length){
+    $('.message-box').remove();
+ } // удаление textarea
+
+$(function(){
+    $('.block').on('click', function(){
+            $('.accordion').prop('disabled' , true)
+    });
+    });
+
+$(function(){
+    $('.block').on('click', function(){
+            $('.accordion').prop('disabled' , true)
+    });
+    });
+
+    window.addEventListener("load",function(){
+});
+
+    $(".accordion").click(function() {
+        $(".accordion").toggleClass("transparent");
+        $(".panel").slideToggle("fast");
+    });
+</script>
